0ca04a1dfb5230da134c5950a58166132d89ff90
Richard Mendoza <rmogollon130@unab.edu.co>
2023-11-30 23:43:30 -0500

    Adding model

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pickle
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
@@ -51,34 +50,6 @@ prediction = None
 if pred_clicked:
     model = load_model()
     prediction = model.predict()
-
-
-'''
-def plot_series(series, y=None, y_pred=None, y_pred_std=None, x_label="$t$", y_label="$x$"):
-    r, c = 3, 5
-    fig, axes = plt.subplots(nrows=r, ncols=c, sharey=True, sharex=True, figsize=(20, 10))
-    for row in range(r):
-        for col in range(c):
-            plt.sca(axes[row][col])
-            ix = col + row * c
-            plt.plot(series[ix, :], ".-")
-            if y is not None:
-                plt.plot(range(len(series[ix, :]), len(series[ix, :]) + len(y[ix])), y[ix], "bx", markersize=10)
-            if y_pred is not None:
-                plt.plot(range(len(series[ix, :]), len(series[ix, :]) + len(y_pred[ix])), y_pred[ix], "ro")
-            if y_pred_std is not None:
-                plt.plot(range(len(series[ix, :]), len(series[ix, :]) + len(y_pred[ix])), y_pred[ix] + y_pred_std[ix])
-                plt.plot(range(len(series[ix, :]), len(series[ix, :]) + len(y_pred[ix])), y_pred[ix] - y_pred_std[ix])
-            plt.grid(True)
-            plt.hlines(0, 0, 100, linewidth=1)
-            plt.axis([0, len(series[ix, :]) + len(y[ix]), -1, 1])
-            if x_label and row == r - 1:
-                plt.xlabel(x_label, fontsize=16)
-            if y_label and col == 0:
-                plt.ylabel(y_label, fontsize=16, rotation=0)
-    return plt
-'''
-
 
 if file:
     df = pd.read_excel(file)
