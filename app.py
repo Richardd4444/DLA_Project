dbd5cc219a01a6a884f3a584a5685b4d5345aa81
Richard Mendoza <rmogollon130@unab.edu.co>
2023-11-30 23:23:31 -0500

    Adding model

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 @st.cache_resource
 def load_model():
-    with open('models/modelLSTM.pkl', 'rb') as model_file:
+    with open('models/modelLSTM1.pkl', 'rb') as model_file:
         model_pkl = pickle.load(model_file)
     return model_pkl
 
@@ -15,6 +15,7 @@ def load_model():
 st.set_page_config(page_title="Proyecto final DLA", page_icon=":robot_face:")
 
 st.markdown("# Proyecto final DLA :notebook:")
+st.markdown("## Predicción de kilometros a recorrer por una persona :runner:")
 st.markdown("---")
 st.markdown("Este proyecto busca predecir el valor siguiente al último de una serie de tiempo"
             "utilizando una red neuronal recurrente, aplicando lo aprendido en la materia de "
@@ -22,19 +23,25 @@ st.markdown("Este proyecto busca predecir el valor siguiente al último de una s
 
 st.markdown("## Aquí cargamos series de tiempo")
 st.markdown("---")
-st.markdown("La serie de tiempo a cargar debe estar en formato xlsx, que será posteriormente"
-            "utilizado para realizar la predicción mediante la red neuronal recurrente")
+st.markdown("La serie de tiempo a cargar debe estar en formato xlsx, que será posteriormente "
+            "utilizado para realizar la predicción mediante la red neuronal recurrente, el dataframe "
+            "debe tener una columna con nombre 'Date' y una con nombre 'Kilometers' para un "
+            "correcto funcionamiento.")
 
 file = st.file_uploader("Sube un archivo .xlsx", type="xlsx")
 
 if file:
     df = pd.read_excel(file)
-    st.success("El archivo fue cargado con éxito")
+    st.sidebar.success(f"El archivo **{file.name}** fue cargado con éxito")
     st.markdown(f"Datos del archivo **{file.name}**: ")
     st.dataframe(df)
+    st.markdown(f"### Gráfica del dataframe **{file.name}**")
+    st.markdown("---")
+    st.line_chart(df['Kilometers'])
 
 st.markdown("## A predecir!")
 st.markdown("---")
+st.markdown("Presionando el botón **Predecir** podrá realizar la predicción de su serie de tiempo")
 
 pred_btn, clear_btn = st.columns(2)
 pred_clicked = pred_btn.button("Predecir")
